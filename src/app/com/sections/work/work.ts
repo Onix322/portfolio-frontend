be17b0fb111db6a0857a0ff8eadb23fb58b142f6
Alex <alexandru.dobos3@gmail.com>
2025-10-08 10:17:54 +0200

    Modified scroll-service.ts Working on work.ts animation

--- src/app/com/sections/work/work.ts
+++ src/app/com/sections/work/work.ts
@@ -3,8 +3,11 @@ import {Window} from '../../util-com/window/window';
 import {GradientRectangle} from '../../util-com/gradient-rectangle/gradient-rectangle';
 import {Grabber} from '../../../service/grabber/grabber';
 import {gsap} from 'gsap';
-import CSSProperties = gsap.CSSProperties;
-import {relative} from '@angular/compiler-cli';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {CSSPlugin} from 'gsap/CSSPlugin';
+import {ScrollService} from '../../../service/scroll/scroll-service';
+import TweenVars = gsap.TweenVars;
+import {compileOpaqueAsyncClassMetadata} from '@angular/compiler';
 
 @Component({
   selector: 'app-work',
@@ -20,18 +23,33 @@ export class Work implements AfterViewInit{
   @ViewChild('window', {read: ElementRef<HTMLElement>})
   private windowRef!: ElementRef<HTMLElement>
 
-  private grabber: Grabber;
+  @ViewChild('workSection', {read: ElementRef<HTMLElement>})
+  private workSectionRef!: ElementRef<HTMLElement>
 
-  constructor(grabber: Grabber) {
+  @ViewChild('workSectionProjects', {read: ElementRef<HTMLElement>})
+  private workSectionProjectsRef!: ElementRef<HTMLElement>
+
+  private grabber: Grabber;
+  private scrollService: ScrollService;
+
+  constructor(grabber: Grabber, scrollService: ScrollService) {
+    gsap.registerPlugin(ScrollTrigger, CSSPlugin)
     this.grabber = grabber
+    this.scrollService = scrollService
   }
 
   ngAfterViewInit() {
+    const workSection = this.workSectionRef.nativeElement
+    const workSectionProjects = this.workSectionProjectsRef.nativeElement
+
     this.grabber.request<ElementRef<HTMLElement>>("windowGradient")
-      .then((res) => {
-        // the code for edit elements
-        this.animateWindow(res.nativeElement)
+      .then((window) => {
+        this.animateWindow(window.nativeElement)
       })
+
+    this.animateWorkSection(workSection)
+    this.animateWorkSectionProjects(workSectionProjects, workSection)
+
   }
 
   private animateWindow(window: HTMLElement) {
@@ -41,15 +59,53 @@ export class Work implements AfterViewInit{
       scrollTrigger: {
         scrub: true,
         pin: true,
-        start: '10%',
-        end: `30%`,
-        markers: true
+        start: 'top',
+        end: `bottom bottom`,
+        markers: false
       },
       position: "relative",
       // pos - width - padding - border
       x: -winRect.x - (winRect.width + 22.5 + 4),
       y: winRect.y - winRect.height / 2 + 22.5 + 4,
-      duration: 1
+      duration: 1,
+      filter: 'none',
+      zIndex: 99
     })
+  }
+
+  private animateWorkSection(workSection: HTMLElement): void {
+
+  }
+
+  private animateWorkSectionProjects(workSectionProjects: HTMLElement, trigger: HTMLElement): void {
+    const rect = workSectionProjects.getBoundingClientRect()
+    const initialY = rect.y
+
+    const scrollTrigger = ScrollTrigger.create({
+      trigger: trigger,
+      markers: true,
+      scrub: true,
+      start: "top 18%",
+      end: "bottom bottom"
+    })
+
+    const outVars: gsap.TweenVars = {
+      y: rect.y - 500,
+      opacity: 0,
+    }
+
+    const inVars: gsap.TweenVars = {
+      y: rect.y - initialY,
+      opacity: 1,
+      duration: 1,
+    }
+
+    const tl = gsap.timeline({
+      scrollTrigger: scrollTrigger
+    })
+
+    tl.from(workSectionProjects, outVars)
+      .to(workSectionProjects, inVars)
+      .to(workSectionProjects, outVars)
   }
 }
--- src/app/service/scroll/scroll-service.ts
+++ src/app/service/scroll/scroll-service.ts
@@ -2,13 +2,21 @@ import {Injectable} from '@angular/core';
 import {gsap} from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import {ScrollSmoother} from 'gsap/ScrollSmoother';
+import {Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrollService {
+  get liveScrollProgress(): Observable<number> {
+    return this._liveScrollProgress.asObservable();
+  }
+
+  private readonly _liveScrollProgress: Subject<number>;
+
   constructor() {
     gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+    this._liveScrollProgress = new Subject()
   }
 
   public createSmoother(elements: ScrollElements) {
@@ -24,9 +32,10 @@ export class ScrollService {
       smooth: 1.8,
       effects: true,
       onUpdate: self => {
+        this._liveScrollProgress.next(self.progress)
         const velocity = self.getVelocity()
         gsap.set(self.content(), {
-          transformOrigin: 'center center',
+          transformOrigin: 'top top',
           skewY: gsap.utils.clamp(-5, 5, velocity / 500),
           filter: 'blur(' + gsap.utils.clamp(0, 5, Math.abs(velocity) / 200) + 'px)',
           duration: 1,
