44f7e24b42db0c766c2bbd1cbf734c789fe2ba22
Alex <alexandru.dobos3@gmail.com>
2025-10-10 09:54:16 +0200

    modified work animation

--- src/app/com/sections/work/work.ts
+++ src/app/com/sections/work/work.ts
@@ -18,8 +18,9 @@ import {ScrollService} from '../../../service/scroll/scroll-service';
 })
 export class Work implements AfterViewInit{
 
-  @ViewChild('window', {read: ElementRef<HTMLElement>})
-  private windowRef!: ElementRef<HTMLElement>
+  @ViewChild('workDetails', {read: ElementRef<HTMLElement>})
+  private workDetails!: ElementRef<HTMLElement>
+
 
   @ViewChild('workSection', {read: ElementRef<HTMLElement>})
   private workSectionRef!: ElementRef<HTMLElement>
@@ -43,35 +44,45 @@ export class Work implements AfterViewInit{
     this.grabber.request<ElementRef<HTMLElement>>("windowGradient")
       .then((window) => {
         this.animateWindow(window.nativeElement)
+        this.animateWorkSectionProjectsChildren(workSectionProjects, window.nativeElement)
       })
 
-    this.animateWorkSectionProjectsChildren(workSectionProjects)
   }
 
+  //rethink
   private animateWindow(window: HTMLElement) {
-    const winRect = this.windowRef.nativeElement.getBoundingClientRect()
+    const workDetailsRect = this.workDetails.nativeElement.getBoundingClientRect()
+    const winRect = window.getBoundingClientRect()
+    // pos - width - padding - border
+    const heightMiddle = (winRect.height / 2 - 22.5 - 4)
+    const widthMiddle = (winRect.width + 22.5 + 4)
 
-    gsap.to(window, {
-      scrollTrigger: {
-        scrub: true,
-        pin: true,
-        start: 'top',
-        end: `bottom bottom`,
-        markers: false
-      },
-      position: "relative",
-      // pos - width - padding - border
-      x: -winRect.x - (winRect.width + 22.5 + 4),
-      y: winRect.y - winRect.height / 2 + 22.5 + 4,
-      duration: 1,
-      filter: 'none',
-      zIndex: 99
-    })
+    // gsap.to(window, {
+    //   scrollTrigger: {
+    //     scrub: true,
+    //     pin: true,
+    //     start: 'top',
+    //     end: `bottom bottom`,
+    //     markers: false
+    //   },
+    //   position: "relative",
+    //   x: -winRect.x - widthMiddle,
+    //   y: winRect.y - heightMiddle,
+    //   duration: 1,
+    //   filter: 'none',
+    //   zIndex: 99
+    // })
   }
 
-  private animateWorkSectionProjectsChildren(workSectionProjects: HTMLElement) {
+  private animateWorkSectionProjectsChildren(workSectionProjects: HTMLElement, window: HTMLElement) {
 
     const children = Array.from(workSectionProjects.children)
+
+    const workDetailsRect = this.workDetails.nativeElement.getBoundingClientRect()
+    const winRect = window.getBoundingClientRect()
+    const heightMiddle = (winRect.height / 2 - 22.5 - 4)
+    const widthMiddle = (winRect.width + 22.5 + 4)
+
 
     children.forEach(child => {
       gsap.set(child, {
@@ -82,23 +93,50 @@ export class Work implements AfterViewInit{
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: child,
-          start: "top 70%",
-          end: "bottom 40%",
+          start: "top 60%",
+          end: "bottom 30%",
           scrub: true,
           markers: true,
-        }
+        },
+        defaults: {
+          ease: "power4.out",
+          delay: 1,
+          duration: 10
+        },
       })
 
-      tl.to(child, {
+      tl.to(window, {
+        x: -(workDetailsRect.x) - widthMiddle,
+        y: child.getBoundingClientRect().y - heightMiddle,
+        opacity: 1,
+      }).to(child, {
         x: 0,
         opacity: 1,
-        ease: "power4.out",
-        duration: 1,
       }).to(child, {
         x: 100,
         opacity: 0,
-        ease: "power4.in",
-        duration: 1,
+      }).to(window, {
+        scrollTrigger: {
+          trigger: children[children.length - 1],
+          markers: false,
+          scrub: true,
+          start: "top 15%",
+          end: "top 0%"
+        },
+        opacity: 0,
+        rotateZ: -20,
+        display: 'none',
+        position: 'relative',
+        left: -100,
+      }).to(children[children.length - 1], {
+        scrollTrigger: {
+          trigger: children[children.length - 1],
+          markers: false,
+          scrub: true,
+          start: "top 15%",
+          end: "top 0%"
+        },
+        rotateZ: 20,
       })
     })
   }
